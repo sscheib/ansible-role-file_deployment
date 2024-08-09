03d3b6dabdc332d5139d77c0f5700ef7e810db6c
Steffen Scheib <steffen@scheib.me>
2024-08-09 15:40:00 +0200

    ci: enforce lower case

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -8,8 +8,8 @@ module.exports = {
         'header-max-length': [2, 'always', 100],
         'subject-case': [
             2,
-            'never',
-            ['sentence-case'],
+            'always',
+            ['lower-case'],
         ],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
